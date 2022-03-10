3cfe878b088419a87ab7d193d7a6a545838c9f3b
XiaoMaShawn <ma2014xiao@hotmail.com>
2022-03-10 17:43:02 -0500

    commit before step9 bundle install

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # session[:user_id]=user.id
+      redirect_to '/'
+    else
+      redirect_to 'signup'
+    end
+  end
+
+  private
+
+  def user_params
+    # .require(:user) point to the form_for :user in views/users/new.html.erb 
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+end

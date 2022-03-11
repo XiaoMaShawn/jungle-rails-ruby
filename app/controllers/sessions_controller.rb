7ed52eb7204ff33c016cd12d76c661eb6098dd50
XiaoMaShawn <ma2014xiao@hotmail.com>
2022-03-10 20:32:43 -0500

    clear up puts in sessions_controller file

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     # If the user exists AND the password entered is correct.
-    puts "params is here, ", params[:email]
     user = User.find_by_email(params[:email])
-    puts "user is here, ", user 
     if user && user.authenticate(params[:password])
      # Save the user id inside the browser cookie. This is how we keep the user logged in when they navigate around our website.
       session[:user_id] = user.id

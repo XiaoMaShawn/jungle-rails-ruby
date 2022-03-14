b00b5c5629d911ec3c1414e1c29e60cd37679adf
XiaoMaShawn <ma2014xiao@hotmail.com>
2022-03-14 18:13:50 -0400

    add test for user, implement user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,21 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, confirmation: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email,password)
+    # If the user exists AND the password entered is correct.
+    @email = email.delete(' ').downcase
+    @user = User.find_by_email(@email)
+    # utilize the authenticate instance method provided by the has_secure_password gem, to check the password
+    if @user && @user.authenticate(password)
+      @user
+    else
+      nil
+    end
+  end
 end
